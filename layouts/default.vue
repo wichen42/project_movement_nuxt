c4e3c6da7baa2a4abb3892deb8bd2c3d80a99997
Wilson Chen <wilsonchen901@gmail.com>
2023-02-21 18:23:33 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- Header -->
+    <div>
+        <div class="flex flex-row">
+            <div class="mx-10 mr-20">
+                <img src="../assets/images/LogoGold.png" alt="logo">
+            </div>            
+            <div class="">
+                <a href="#home">Home</a>
+                <a href="#programs">Programs</a>
+                <a href="#testimonials">Testimonials</a>
+                <a href="contact">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Body -->
+    <div>
+        <slot/>
+    </div>
+
+    <!-- Footer -->
+    <div>
+        <p>footer</p>
+    </div>
+</template>
