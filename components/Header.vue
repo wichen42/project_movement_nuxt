1bb55f9599536391a911a6d1c6225f0dadf0f7b6
Wilson Chen <wilsonchen901@gmail.com>
2023-03-03 14:59:55 -0500

    change header style on scroll

--- components/Header.vue
+++ components/Header.vue
@@ -5,10 +5,10 @@
                 <img class="mr-10 pl-16" :src="logoStyle" alt="logo">
             </div>            
             <div class="h-full w-full flex justify-between items-center">
-                <button :class="anchorStyle" @click="handleHome" class="anchor">Home</button>
-                <button :class="anchorStyle" @click="handleProgram" class="anchor">Programs</button>
-                <button :class="anchorStyle" @click="handleTestimonial" class="anchor">Testimonials</button>
-                <button :class="anchorStyle" @click="handleContact"  class="anchor">Contact Us</button>
+                <button :class="homeStyle" @click="handleHome" class="anchor">Home</button>
+                <button :class="programStyle" @click="handleProgram" class="anchor">Programs</button>
+                <button :class="testimonialStyle" @click="handleTestimonial" class="anchor">Testimonials</button>
+                <button :class="contactStyle" @click="handleContact"  class="anchor">Contact Us</button>
             </div>
         </div>
 
@@ -23,6 +23,10 @@ export default {
     data() {
         return {
             scrolled: false,
+            home: false,
+            program: false,
+            testimonial: false,
+            contact: false
         }
     },
     mounted() {
@@ -31,6 +35,10 @@ export default {
     methods: {
         handleScroll() {
             this.scrolled = window.scrollY > 0
+            this.home = window.scrollY === 0
+            this.program = window.scrollY >= 800 && window.scrollY < 2100
+            this.testimonial = window.scrollY >= 2100 && window.scrollY < 2800
+            this.contact = window.scrollY >= 2800
         },
         handleHome() {
             window.scrollTo({
@@ -65,6 +73,18 @@ export default {
         anchorStyle() {
             return this.scrolled ? 'text-black' : ''
         },
+        homeStyle() {
+            return this.scrolled ? 'text-black' : 'text-[#D4AF37]'
+        },  
+        programStyle() {
+            return this.scrolled ? (this.program ? 'text-[#D4AF37]' : 'text-black') : ''
+        },
+        testimonialStyle() {
+            return this.scrolled ? (this.testimonial? 'text-[#D4AF37]' : 'text-black') : ''
+        },  
+        contactStyle() {
+            return this.scrolled ? (this.contact ? 'text-[#D4AF37]' : 'text-black') : ''
+        },  
         logoStyle() {
             if (this.scrolled) {
                 return '_nuxt/assets/images/LogoBlack.png'
